e075b68a5d5bcba340950ddfd0d568b11ea8b0cf
Alfan Nur Fauzan <alpancs@gmail.com>
2015-06-16 10:38:22 +0700

    remove garbages

---
+++ settings.py
@@ -0,0 +1,9 @@
+# ENGINE_STRING = 'mysql://@n@localhost/ionstreamer'
+
+# SECRET_KEY = 'adFEaef3rqwfWrASDFa325afASfas'
+
+# PORT = 8888
+
+# API_ENDPOINT = "http://128.199.120.29:8200/api/v1"
+
+# SNAPSHOT_SCRIPT = "snapshot.py"
