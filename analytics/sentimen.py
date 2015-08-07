f53c1f879f20919a121902f9841d2398d5a6412b
Alfan Nur Fauzan <alpancs@gmail.com>
2015-08-07 11:08:55 +0700

    Changes to be committed: 	modified:   analytics/sentimen.py 	modified:   templates/classified.html 	modified:   templates/learn.html

--- analytics/sentimen.py
+++ analytics/sentimen.py
@@ -1,11 +1,11 @@
+from os.path import abspath, isfile
 import sys
-import database.dbresult as dbr
-
-from os.path import abspath
-
 sys.path.append(abspath(''))
 
-def getclassified(class = '', size = 10, offset = 0):
-  print class
+import database.dbresult as dbr
+
+def getclassified(kelas= '', size = 10, offset = 0):
   s = dbr.get_search_instance('LEARN', enc = False).params(size = size, from_ = offset)
   return s.execute().hits
+
+print getclassified(kelas= 'haha')
