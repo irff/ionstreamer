2e6028dd1d855874aac688f10162802676d65856
Alfan Nur Fauzan <alpancs@gmail.com>
2015-08-25 21:37:32 +0700

    reduce instance of streamers

--- run.sh
+++ run.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 python router.py &
-python streamer/tweet_streamer.py 0 &
-sleep 1
-python streamer/tweet_streamer.py 1 &
-sleep 1
-python streamer/tweet_streamer.py 2 &
-sleep 1
-python streamer/tweet_streamer.py 3 &
-sleep 1
-python streamer/tweet_streamer.py 4 &
+python streamer/tweet_streamer.py &
+python streamer/tweet_streamer.py &
