30df94cd447b330809b7b48f1ae01c906aed5777
Alfan Nur Fauzan <alpancs@gmail.com>
2015-08-26 10:23:51 +0700

    Changes to be committed: 	modified:   static/js/app.js

--- static/js/app.js
+++ static/js/app.js
@@ -93,18 +93,14 @@ var BASE_URL = '';
       kword = $scope.keyword.trim().toLowerCase();
       if(kword == "") return false;
       $scope.is_sending_kw = true;
-      $http.post(BASE_URL + '/api/stream' , {keyword: kword, status: 'active', last_modified: '2000-01-01 00:00:00'})
+      $http.post(BASE_URL + '/api/stream' , {keyword: kword, status: 'active', last_modified: '2000-01-01 00:00'})
       .success(function(){
-        $http.get(BASE_URL + '/api/summary')
-        .success(function(r){
-          $scope.is_sending_kw = false;
-          $scope.keyword = '';
-          refresh();
-        })
-        .error(function(){
-          $scope.is_sending_kw = false;
-          refresh();
-        });
+        $scope.is_sending_kw = false;
+        $scope.keyword = '';
+        refresh();
+      })
+      .error(function(){
+        $scope.is_sending_kw = false;
       });
     };
   });
