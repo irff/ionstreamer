d74fc5e133b807b25b34023249b6f205daf4bc26
Alfan Nur Fauzan <alpancs@gmail.com>
2015-09-03 23:14:36 +0700

    Changes to be committed: 	modified:   router.py

--- router.py
+++ router.py
@@ -15,8 +15,7 @@ app.permanent_session_lifetime = timedelta(days=1001)
 
 
 def islogin():
-  print request.remote_addr
-  return session.get('username') == 'langgar'
+  return session.get('username') == 'langgar' or request.remote_addr == '127.0.0.1'
 
 
 
